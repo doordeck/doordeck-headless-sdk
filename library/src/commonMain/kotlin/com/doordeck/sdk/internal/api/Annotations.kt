dcc6ecce7df4f5de42cdbf3a2acd122ad6ee5433
Bernat <bernatgarciacarbo@gmail.com>
2024-06-12 23:03:28 +0200

    wip

--- library/src/commonMain/kotlin/com/doordeck/sdk/internal/api/Annotations.kt
+++ library/src/commonMain/kotlin/com/doordeck/sdk/internal/api/Annotations.kt
@@ -2,10 +2,8 @@ package com.doordeck.sdk.internal.api
 
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.BINARY)
-@RequiresOptIn(message = "This library API is only available to users with Doordeck issued auth tokens.")
 annotation class DoordeckOnly
 
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.BINARY)
-@RequiresOptIn(message = "This library API is only available to users that are lock administrators.")
 annotation class SiteAdmin
