6ba9a80fbb96ab1d1f53bceec4d1c410f6a06532
Michael Barnwell <michael@doordeck.com>
2024-06-20 14:46:53 +0200

    Fixing apple target

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -16,9 +16,6 @@ kotlin {
             }
         }
     }
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
     macosX64()
     macosArm64()
     js(IR) {
@@ -70,14 +67,7 @@ kotlin {
             }
         }
 
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by getting {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
+        val appleMain by getting {
             dependencies {
                 implementation(libs.ktor.client.darwin)
             }
