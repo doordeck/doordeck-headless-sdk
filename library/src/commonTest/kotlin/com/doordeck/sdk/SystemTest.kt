1ebe5081022971509b03a2afff16c7da72e61859
Bernat <bernatgarciacarbo@gmail.com>
2024-06-26 14:28:08 +0200

    wip

--- library/src/commonTest/kotlin/com/doordeck/sdk/SystemTest.kt
+++ library/src/commonTest/kotlin/com/doordeck/sdk/SystemTest.kt
@@ -2,7 +2,7 @@ package com.doordeck.sdk
 
 open class SystemTest {
 
-    private val TEST_AUTH_TOKEN = getEnvironmentVariable("TEST_AUTH_TOKEN") ?: ""
+    val TEST_AUTH_TOKEN = getEnvironmentVariable("TEST_AUTH_TOKEN") ?: ""
     val TEST_MAIN_APPLICATION_OWNER = getEnvironmentVariable("TEST_MAIN_APPLICATION_OWNER") ?: ""
     val TEST_NEW_APPLICATION_OWNER = getEnvironmentVariable("TEST_NEW_APPLICATION_OWNER") ?: ""
 }
