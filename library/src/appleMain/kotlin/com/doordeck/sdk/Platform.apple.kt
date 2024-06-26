333fc6a17207a182fc4dfeb80edbb83b9f9b7560
Bernat <bernatgarciacarbo@gmail.com>
2024-06-26 22:20:02 +0200

    wip

--- library/src/appleMain/kotlin/com/doordeck/sdk/Platform.apple.kt
+++ library/src/appleMain/kotlin/com/doordeck/sdk/Platform.apple.kt
@@ -1,10 +1,12 @@
 package com.doordeck.sdk
 
 import kotlinx.coroutines.CoroutineScope
+import platform.Foundation.NSProcessInfo
 
 actual fun <T> runBlocking(block: suspend CoroutineScope.() -> T): T =
     kotlinx.coroutines.runBlocking { block() }
 
 actual fun getPlatform(): PlatformType = PlatformType.APPLE
 
-actual fun getEnvironmentVariable(name: String): String? = null // TODO
+actual fun getEnvironmentVariable(name: String): String? =
+    NSProcessInfo.processInfo.environment[name]
