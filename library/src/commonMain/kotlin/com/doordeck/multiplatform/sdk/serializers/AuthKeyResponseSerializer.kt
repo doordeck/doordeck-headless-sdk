ffddcf230214c59553b9bb012819f7caba7f8852
Bernat <bernatgarciacarbo@gmail.com>
2024-07-22 16:36:33 +0200

    [skip actions]

--- library/src/commonMain/kotlin/com/doordeck/multiplatform/sdk/serializers/AuthKeyResponseSerializer.kt
+++ library/src/commonMain/kotlin/com/doordeck/multiplatform/sdk/serializers/AuthKeyResponseSerializer.kt
@@ -1,7 +1,9 @@
 package com.doordeck.multiplatform.sdk.serializers
 
 import com.doordeck.multiplatform.sdk.api.responses.AuthKeyResponse
+import com.doordeck.multiplatform.sdk.api.responses.EcKeyResponse
 import com.doordeck.multiplatform.sdk.api.responses.Ed25519KeyResponse
+import com.doordeck.multiplatform.sdk.api.responses.RsaKeyResponse
 import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.json.JsonContentPolymorphicSerializer
 import kotlinx.serialization.json.JsonElement
@@ -10,9 +12,11 @@ import kotlinx.serialization.json.jsonPrimitive
 
 object AuthKeyResponseSerializer : JsonContentPolymorphicSerializer<AuthKeyResponse>(AuthKeyResponse::class) {
     override fun selectDeserializer(element: JsonElement): DeserializationStrategy<AuthKeyResponse> {
-        return when (element.jsonObject["alg"]?.jsonPrimitive?.content) {
-            "EdDSA" -> Ed25519KeyResponse.serializer()
-            else -> throw Exception("Unknown Module: key 'type' not found or does not matches any module type")
+        return when (element.jsonObject["kty"]?.jsonPrimitive?.content) {
+            "OKP" -> Ed25519KeyResponse.serializer()
+            "RSA" -> RsaKeyResponse.serializer()
+            "EC" -> EcKeyResponse.serializer()
+            else -> throw Exception("Unable to deserialize auth key: key 'kty' not found or does not matches any module type")
         }
     }
 }
