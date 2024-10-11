3ebf35e3bf0898238130fb2f225b9baa5dd50b83
Bernat <bernatgarciacarbo@gmail.com>
2024-10-11 15:40:13 +0200

    wip

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,3 @@ plugins {
     id("com.netflix.nebula.release") version "19.0.8"
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.16.3"
 }
-
-nebulaRelease {
-    releaseBranchPatterns.add("npm-publish")
-}
