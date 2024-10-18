b20fb9914bc7568c6ed43144666da73fb75b8b73
Bernat <bernatgarciacarbo@gmail.com>
2024-10-18 17:30:28 +0200

    removing kmmBridge [skip actions]

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.kotlinxSerialization).apply(false)
     alias(libs.plugins.kotlinCocoapods).apply(false)
-    alias(libs.plugins.kmmBridge).apply(false)
     id("com.netflix.nebula.release") version "19.0.8"
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.16.3"
 }
