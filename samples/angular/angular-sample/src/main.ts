3e1abe3b05b1c6374e991ddf2a0e3d0a5754449e
Bernat Carbó <41543253+BernatCarbo@users.noreply.github.com>
2024-10-25 14:36:32 +0200

    Js sample (#86)
    * Js sample

---
+++ samples/angular/angular-sample/src/main.ts
@@ -0,0 +1,19 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import doordeck from '@doordeck/doordeck-headless-sdk';
+
+const apiEnvironment =  doordeck.com.doordeck.multiplatform.sdk.api.model.ApiEnvironment;
+export const doordeckSDK = doordeck.com.doordeck.multiplatform.sdk.KDoordeckFactory.initialize(apiEnvironment.DEV);
+
+// Utils
+export const doordeckCrypto = doordeck.com.doordeck.multiplatform.sdk.util.Crypto;
+
+// Resources
+export const sitesResource = doordeck.com.doordeck.multiplatform.sdk.api.sites();
+export const accountResource = doordeck.com.doordeck.multiplatform.sdk.api.account();
+export const accountlessResource = doordeck.com.doordeck.multiplatform.sdk.api.accountless();
+export const lockOperationResource = doordeck.com.doordeck.multiplatform.sdk.api.lockOperations();
+
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
