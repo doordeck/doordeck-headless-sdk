28cc40b2bb23abf26186b98e9f412123ca6a1db8
Bernat <bernatgarciacarbo@gmail.com>
2024-10-28 14:06:41 +0100

    Crypto kit

---
+++ library/src/appleMain/kotlin/com/doordeck/multiplatform/sdk/crypto/CryptoHelper.kt
@@ -0,0 +1,21 @@
+package com.doordeck.multiplatform.sdk.crypto
+
+import Foundation
+import CryptoKit
+
+@objc public class CryptoHelper: NSObject {
+
+    // Generate Ed25519 Key Pair
+    @objc public func generateEd25519KeyPair() -> [String: String] {
+        let privateKey = Curve25519.Signing.PrivateKey()
+        let publicKey = privateKey.publicKey
+
+                let privateKeyBase64 = privateKey.rawRepresentation.base64EncodedString()
+        let publicKeyBase64 = publicKey.rawRepresentation.base64EncodedString()
+
+        return [
+            "privateKey": privateKeyBase64,
+             "publicKey": publicKeyBase64
+        ]
+    }
+}
