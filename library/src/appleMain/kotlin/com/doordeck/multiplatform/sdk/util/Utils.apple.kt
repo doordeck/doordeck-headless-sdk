d3f9aad193a70883035189f8226f9327ebe67928
Bernat <bernatgarciacarbo@gmail.com>
2024-10-28 23:10:24 +0100

    wip

--- library/src/appleMain/kotlin/com/doordeck/multiplatform/sdk/util/Utils.apple.kt
+++ library/src/appleMain/kotlin/com/doordeck/multiplatform/sdk/util/Utils.apple.kt
@@ -1,7 +1,9 @@
 package com.doordeck.multiplatform.sdk.util
 
+import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.usePinned
 import platform.Foundation.NSData
+import platform.posix.memcpy
 
 fun NSData.toByteArray(): ByteArray = ByteArray(length.toInt()).apply {
     usePinned {
