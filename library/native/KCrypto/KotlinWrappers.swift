2d8c232d970a49ecfa1319ca77e9e6135eb035ab
Bernat <bernatgarciacarbo@gmail.com>
2024-10-28 16:23:46 +0100

    wip

--- library/native/KCrypto/KotlinWrappers.swift
+++ library/native/KCrypto/KotlinWrappers.swift
@@ -4,7 +4,7 @@ import CryptoKit
 @objc public class KCrypto : NSObject {
 
     // Generate Ed25519 Key Pair
-    @objc public func generateEd25519KeyPair() -> [String: String] {
+    @objc public class func generateEd25519KeyPair() -> [String: String] {
         let privateKey = Curve25519.Signing.PrivateKey()
         let publicKey = privateKey.publicKey
 
