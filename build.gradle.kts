d1c36003acd246eb105d424daace080af03326a8
Bernat Carbó <41543253+BernatCarbo@users.noreply.github.com>
2024-11-14 17:23:12 +0100

    Update build.gradle.kts
    Co-authored-by: Michael Barnwell <michael@doordeck.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.16.3"
 }
 
-group = "com.doordeck" // Required by nexusPublishing
+group = "com.doordeck"
 
 nexusPublishing {
     repositories {
