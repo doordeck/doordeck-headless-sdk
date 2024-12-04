cfba10d85da6b44e4aa38108e0d472d43e5a0a64
Bernat <bernatgarciacarbo@gmail.com>
2024-12-04 14:44:46 +0100

    JwtUtils

---
+++ doordeck-sdk/src/commonMain/kotlin/com/doordeck/multiplatform/sdk/util/JwtUtils.kt
@@ -0,0 +1,31 @@
+package com.doordeck.multiplatform.sdk.util
+
+import com.doordeck.multiplatform.sdk.JSON
+import io.ktor.util.decodeBase64String
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import kotlin.js.JsExport
+import kotlin.time.Duration.Companion.days
+
+@JsExport
+object JwtUtils {
+    private const val TOKEN_EXPIRE_AT_FIELD = "exp"
+
+    private fun getClaims(token: String): Map<String, String> {
+        val parts = token.split(".")
+        if (parts.size != 3) {
+            return emptyMap()
+        }
+        val decodedPayload = parts[1].decodeBase64String()
+        val json = JSON.parseToJsonElement(decodedPayload)
+        return json.jsonObject.map { it.key to it.value.jsonPrimitive.content }.toMap()
+    }
+
+    fun String.isJwtTokenAboutToExpire(): Boolean {
+        return getClaims(this)[TOKEN_EXPIRE_AT_FIELD]?.let {
+            Clock.System.now() >= Instant.fromEpochSeconds(it.toLong()) - 1.days
+        } ?: true
+    }
+}
