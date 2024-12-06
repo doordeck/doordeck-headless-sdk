e82b0a1a17e7c1f8f88c35765455c6ae0f7524c5
bernat <bernatgarciacarbo@gmail.com>
2024-12-06 16:44:40 +0100

    added missing @DoordeckOnly at fusion interfaces [skip actions]

--- doordeck-sdk/src/appleMain/kotlin/com/doordeck/multiplatform/sdk/api/FusionResource.apple.kt
+++ doordeck-sdk/src/appleMain/kotlin/com/doordeck/multiplatform/sdk/api/FusionResource.apple.kt
@@ -5,32 +5,41 @@ import com.doordeck.multiplatform.sdk.api.responses.DoorStateResponse
 import com.doordeck.multiplatform.sdk.api.responses.FusionLoginResponse
 import com.doordeck.multiplatform.sdk.api.responses.IntegrationConfigurationResponse
 import com.doordeck.multiplatform.sdk.api.responses.IntegrationTypeResponse
+import com.doordeck.multiplatform.sdk.internal.api.DoordeckOnly
 import com.doordeck.multiplatform.sdk.internal.api.FusionClient
 import com.doordeck.multiplatform.sdk.internal.api.FusionResourceImpl
 import org.koin.mp.KoinPlatform.getKoin
 
 actual interface FusionResource {
+    @DoordeckOnly
     @Throws(Exception::class)
     suspend fun login(email: String, password: String): FusionLoginResponse
 
+    @DoordeckOnly
     @Throws(Exception::class)
     suspend fun getIntegrationType(): IntegrationTypeResponse
 
+    @DoordeckOnly
     @Throws(Exception::class)
     suspend fun getIntegrationConfiguration(type: String): List<IntegrationConfigurationResponse>
 
+    @DoordeckOnly
     @Throws(Exception::class)
     suspend fun enableDoor(name: String, siteId: String, controller: Fusion.LockController)
 
+    @DoordeckOnly
     @Throws(Exception::class)
     suspend fun deleteDoor(deviceId: String)
 
+    @DoordeckOnly
     @Throws(Exception::class)
     suspend fun getDoorStatus(deviceId: String): DoorStateResponse
 
+    @DoordeckOnly
     @Throws(Exception::class)
     suspend fun startDoor(deviceId: String)
 
+    @DoordeckOnly
     @Throws(Exception::class)
     suspend fun stopDoor(deviceId: String)
 }
--- doordeck-sdk/src/mingwMain/kotlin/com/doordeck/multiplatform/sdk/api/FusionResource.mingw.kt
+++ doordeck-sdk/src/mingwMain/kotlin/com/doordeck/multiplatform/sdk/api/FusionResource.mingw.kt
@@ -5,26 +5,58 @@ import com.doordeck.multiplatform.sdk.api.responses.DoorStateResponse
 import com.doordeck.multiplatform.sdk.api.responses.FusionLoginResponse
 import com.doordeck.multiplatform.sdk.api.responses.IntegrationConfigurationResponse
 import com.doordeck.multiplatform.sdk.api.responses.IntegrationTypeResponse
+import com.doordeck.multiplatform.sdk.internal.api.DoordeckOnly
 import com.doordeck.multiplatform.sdk.internal.api.FusionClient
 import com.doordeck.multiplatform.sdk.internal.api.FusionResourceImpl
 import org.koin.mp.KoinPlatform.getKoin
 
 actual interface FusionResource {
+    @DoordeckOnly
     fun login(email: String, password: String): FusionLoginResponse
+
+    @DoordeckOnly
     fun loginJson(data: String): String
+
+    @DoordeckOnly
     fun getIntegrationType(): IntegrationTypeResponse
+
+    @DoordeckOnly
     fun getIntegrationTypeJson(): String
+
+    @DoordeckOnly
     fun getIntegrationConfiguration(type: String): List<IntegrationConfigurationResponse>
+
+    @DoordeckOnly
     fun getIntegrationConfigurationJson(data: String): String
+
+    @DoordeckOnly
     fun enableDoor(name: String, siteId: String, controller: Fusion.LockController)
+
+    @DoordeckOnly
     fun enableDoorJson(data: String)
+
+    @DoordeckOnly
     fun deleteDoor(deviceId: String)
+
+    @DoordeckOnly
     fun deleteDoorJson(data: String)
+
+    @DoordeckOnly
     fun getDoorStatus(deviceId: String): DoorStateResponse
+
+    @DoordeckOnly
     fun getDoorStatusJson(data: String): String
+
+    @DoordeckOnly
     fun startDoor(deviceId: String)
+
+    @DoordeckOnly
     fun startDoorJson(data: String)
+
+    @DoordeckOnly
     fun stopDoor(deviceId: String)
+
+    @DoordeckOnly
     fun stopDoorJson(data: String)
 }
 
