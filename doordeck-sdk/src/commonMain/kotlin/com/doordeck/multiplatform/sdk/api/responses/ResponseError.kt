10a80e1f26d17842f4f5dc86da0ce33655204782
Bernat <bernatgarciacarbo@gmail.com>
2025-02-10 17:10:18 +0100

    ResponseError major & minor codes

--- doordeck-sdk/src/commonMain/kotlin/com/doordeck/multiplatform/sdk/api/responses/ResponseError.kt
+++ doordeck-sdk/src/commonMain/kotlin/com/doordeck/multiplatform/sdk/api/responses/ResponseError.kt
@@ -3,7 +3,11 @@ package com.doordeck.multiplatform.sdk.api.responses
 import kotlinx.serialization.Serializable
 
 @Serializable
-internal class ResponseError(
-    val code: Int? = null,
+internal data class ResponseError(
+    val code: Double,
     val message: String? = null
-)
+) {
+    fun getMajorCode(): Int = code.toInt()
+    fun getMinorCode(): Int? = code.toString().substringAfter(".", "")
+        .toIntOrNull().takeIf { it != 0 }
+}
