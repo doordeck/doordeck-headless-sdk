1fa3e83aea39f4bb1e0eec0ecaf3e26ae34d8034
Bernat <bernatgarciacarbo@gmail.com>
2025-02-15 09:57:57 +0100

    wip

---
+++ doordeck-sdk/src/commonMain/kotlin/com/doordeck/multiplatform/sdk/exceptions/SdkExceptions.kt
@@ -0,0 +1,17 @@
+package com.doordeck.multiplatform.sdk.exceptions
+
+import kotlin.js.JsExport
+
+/**
+ * SDK Exceptions
+ */
+@JsExport
+open class SdkException(override val message: String, exception: Throwable? = null) : Exception(message, exception)
+
+@JsExport
+class MissingContextFieldException(override val message: String) : SdkException(message)
+
+class MissingAndroidContextException(override val message: String) : SdkException(message)
+
+@JsExport
+class BatchShareFailedException(override val message: String, userIds: List<String>) : SdkException(message)
