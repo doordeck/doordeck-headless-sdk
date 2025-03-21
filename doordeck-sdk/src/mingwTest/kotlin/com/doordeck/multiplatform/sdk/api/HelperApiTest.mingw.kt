fb497d770ee41d2da8d139fe54e9fb7dc229d3f5
Bernat <bernatgarciacarbo@gmail.com>
2025-03-21 01:16:25 +0100

    wip [skip actions]

--- doordeck-sdk/src/mingwTest/kotlin/com/doordeck/multiplatform/sdk/api/HelperApiTest.mingw.kt
+++ doordeck-sdk/src/mingwTest/kotlin/com/doordeck/multiplatform/sdk/api/HelperApiTest.mingw.kt
@@ -1,22 +1,38 @@
 package com.doordeck.multiplatform.sdk.api
 
-import com.doordeck.multiplatform.sdk.MockTest
+import com.doordeck.multiplatform.sdk.CallbackTest
 import com.doordeck.multiplatform.sdk.TestConstants.DEFAULT_APPLICATION_ID
+import com.doordeck.multiplatform.sdk.UNIT_RESULT_DATA
+import com.doordeck.multiplatform.sdk.capturedCallback
 import com.doordeck.multiplatform.sdk.model.data.UploadPlatformLogoData
+import com.doordeck.multiplatform.sdk.testCallback
 import com.doordeck.multiplatform.sdk.util.Utils.encodeByteArrayToBase64
 import com.doordeck.multiplatform.sdk.util.toJson
+import kotlinx.cinterop.staticCFunction
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.time.Duration.Companion.milliseconds
 
-class HelperApiTest : MockTest() {
+class HelperApiTest : CallbackTest() {
 
     @Test
     fun shouldUploadPlatformLogo() = runTest {
-        HelperApi.uploadPlatformLogo(DEFAULT_APPLICATION_ID, "", byteArrayOf())
-    }
+        runBlocking {
+            // Given
+            val callbackPtr = staticCFunction(::testCallback)
 
-    @Test
-    fun shouldUploadPlatformLogoJson() = runTest {
-        HelperApi.uploadPlatformLogoJson(UploadPlatformLogoData(DEFAULT_APPLICATION_ID, "", byteArrayOf().encodeByteArrayToBase64()).toJson())
+            // When
+            HelperApi.uploadPlatformLogo(
+                data = UploadPlatformLogoData(DEFAULT_APPLICATION_ID, "", byteArrayOf().encodeByteArrayToBase64()).toJson(),
+                callback = callbackPtr
+            )
+            delay(10.milliseconds)
+
+            // Then
+            assertEquals(UNIT_RESULT_DATA, capturedCallback)
+        }
     }
 }
