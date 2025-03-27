423fc5e10fb6678475b598bba937d34627c9426a
Bernat Carbó <41543253+BernatCarbo@users.noreply.github.com>
2025-03-27 17:09:08 +0100

    Bumping dependencies (#165)
    * Bumping dependencies

--- samples/angular/angular-sample/src/app/app.component.ts
+++ samples/angular/angular-sample/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {LoginComponent} from './login/login.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, LoginComponent, DashboardComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular-SDK-Sample';
