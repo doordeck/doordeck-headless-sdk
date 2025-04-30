677450c4701efbc225fb519cfb3853abbfe2f51f
Bernat <bernatgarciacarbo@gmail.com>
2025-04-30 14:42:10 +0200

    wip [skip actions]

---
+++ doordeck-sdk/src/mingwMain/resources/csharp/Wrapper/ISecureStorage.cs
@@ -0,0 +1,52 @@
+using Doordeck.Headless.Sdk.Model;
+
+namespace Doordeck.Headless.Sdk.Wrapper;
+
+public interface ISecureStorage
+{
+    public void SetApiEnvironment(ApiEnvironment apiEnvironment);
+
+        public ApiEnvironment? GetApiEnvironment();
+
+        public void AddCloudAuthToken(string token);
+
+        public string? GetCloudAuthToken();
+
+        public void AddCloudRefreshToken(string token);
+
+        public string? GetCloudRefreshToken();
+
+        public void SetFusionHost(string host);
+
+        public string? GetFusionHost();
+
+        public void AddFusionAuthToken(string token);
+
+        public string? GetFusionAuthToken();
+
+        public void AddPublicKey(byte[] publicKey);
+
+        public byte[]? GetPublicKey();
+
+        public void AddPrivateKey(byte[] privateKey);
+
+        public byte[]? GetPrivateKey();
+
+        public void SetKeyPairVerified(bool isKeyVerified);
+
+        public bool GetKeyPairVerified();
+
+        public void AddUserId(string userId);
+
+        public string? GetUserId();
+
+        public void AddUserEmail(string email);
+
+        public string? GetUserEmail();
+
+        public void AddCertificateChain(List<string> certificateChain);
+
+        public List<string>? GetCertificateChain();
+
+        public void Clear();
+}
