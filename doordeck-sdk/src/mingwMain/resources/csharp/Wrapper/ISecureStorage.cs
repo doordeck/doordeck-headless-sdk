352ace3583c289b2d7747cabfb06dcbae5d67455
Bernat <bernatgarciacarbo@gmail.com>
2025-04-30 17:29:45 +0200

    wip [skip actions]

--- doordeck-sdk/src/mingwMain/resources/csharp/Wrapper/ISecureStorage.cs
+++ doordeck-sdk/src/mingwMain/resources/csharp/Wrapper/ISecureStorage.cs
@@ -6,47 +6,47 @@ public interface ISecureStorage
 {
     public void SetApiEnvironment(ApiEnvironment apiEnvironment);
 
-        public ApiEnvironment? GetApiEnvironment();
+    public ApiEnvironment? GetApiEnvironment();
 
-        public void AddCloudAuthToken(string token);
+    public void AddCloudAuthToken(string token);
 
-        public string? GetCloudAuthToken();
+    public string? GetCloudAuthToken();
 
-        public void AddCloudRefreshToken(string token);
+    public void AddCloudRefreshToken(string token);
 
-        public string? GetCloudRefreshToken();
+    public string? GetCloudRefreshToken();
 
-        public void SetFusionHost(string host);
+    public void SetFusionHost(string host);
 
-        public string? GetFusionHost();
+    public string? GetFusionHost();
 
-        public void AddFusionAuthToken(string token);
+    public void AddFusionAuthToken(string token);
 
-        public string? GetFusionAuthToken();
+    public string? GetFusionAuthToken();
 
-        public void AddPublicKey(byte[] publicKey);
+    public void AddPublicKey(byte[] publicKey);
 
-        public byte[]? GetPublicKey();
+    public byte[]? GetPublicKey();
 
-        public void AddPrivateKey(byte[] privateKey);
+    public void AddPrivateKey(byte[] privateKey);
 
-        public byte[]? GetPrivateKey();
+    public byte[]? GetPrivateKey();
 
-        public void SetKeyPairVerified(bool isKeyVerified);
+    public void SetKeyPairVerified(bool isKeyVerified);
 
-        public bool GetKeyPairVerified();
+    public bool? GetKeyPairVerified();
 
-        public void AddUserId(string userId);
+    public void AddUserId(string userId);
 
-        public string? GetUserId();
+    public string? GetUserId();
 
-        public void AddUserEmail(string email);
+    public void AddUserEmail(string email);
 
-        public string? GetUserEmail();
+    public string? GetUserEmail();
 
-        public void AddCertificateChain(List<string> certificateChain);
+    public void AddCertificateChain(List<string> certificateChain);
 
-        public List<string>? GetCertificateChain();
+    public List<string>? GetCertificateChain();
 
-        public void Clear();
+    public void Clear();
 }
