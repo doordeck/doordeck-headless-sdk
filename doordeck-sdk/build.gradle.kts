2849701f35350f1b7f8a5a7aa7047853e64f6444
Bernat <bernatgarciacarbo@gmail.com>
2025-05-01 23:09:40 +0200

    test

--- doordeck-sdk/build.gradle.kts
+++ doordeck-sdk/build.gradle.kts
@@ -484,7 +484,4 @@ classifiers = [
 "Issue tracker" = "${pypiPublish.issues}"
 [tool.setuptools]
 package-data = { "${pypiPublish.packageName}" = ["_doordeck_headless_sdk.pyd", "${nugetPublish.packageName}.dll"] }
-[tool.pyright]
-reportMissingImports = false
-reportAttributeAccessIssue = false
 """.trimIndent()
