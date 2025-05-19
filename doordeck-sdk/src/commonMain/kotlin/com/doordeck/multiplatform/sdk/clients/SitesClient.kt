5c253d36cb708f40d782cd0ff01c2329ced8a6ca
Bernat <bernatgarciacarbo@gmail.com>
2025-05-19 23:07:45 +0200

    Documentation

--- doordeck-sdk/src/commonMain/kotlin/com/doordeck/multiplatform/sdk/clients/SitesClient.kt
+++ doordeck-sdk/src/commonMain/kotlin/com/doordeck/multiplatform/sdk/clients/SitesClient.kt
@@ -1,6 +1,7 @@
 package com.doordeck.multiplatform.sdk.clients
 
 import com.doordeck.multiplatform.sdk.CloudHttpClient
+import com.doordeck.multiplatform.sdk.exceptions.SdkException
 import com.doordeck.multiplatform.sdk.model.network.Paths
 import com.doordeck.multiplatform.sdk.model.responses.SiteLocksResponse
 import com.doordeck.multiplatform.sdk.model.responses.SiteResponse
@@ -8,10 +9,16 @@ import com.doordeck.multiplatform.sdk.model.responses.UserForSiteResponse
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 
+/**
+ * Internal implementation of the sites API client.
+ * Handles all network requests related to sites.
+ */
 internal object SitesClient {
-
     /**
-     * List sites
+     * Retrieves all sites accessible to the current user.
+     *
+     * @return List of [SiteResponse].
+     * @throws SdkException if an unexpected error occurs while processing the request.
      *
      * @see <a href="https://developer.doordeck.com/docs/#sites">API Doc</a>
      */
@@ -20,7 +27,11 @@ internal object SitesClient {
     }
 
     /**
-     * Get locks for site
+     * Retrieves all user-accessible locks in the specified site.
+     *
+     * @param siteId The site ID.
+     * @return List of [SiteLocksResponse].
+     * @throws SdkException if an unexpected error occurs while processing the request.
      *
      * @see <a href="https://developer.doordeck.com/docs/#get-locks-for-site">API Doc</a>
      */
@@ -29,7 +40,12 @@ internal object SitesClient {
     }
 
     /**
-     * Get users for a site
+     * Retrieves users for the specified site, filtered to only include locks
+     * where the current user has administrator privileges.
+     *
+     * @param siteId The site ID.
+     * @return List of [UserForSiteResponse].
+     * @throws SdkException if an unexpected error occurs while processing the request.
      *
      * @see <a href="https://developer.doordeck.com/docs/#get-users-for-a-site">API Doc</a>
      */
