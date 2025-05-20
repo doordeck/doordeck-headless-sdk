4193036db9f14b248d66db6399350d8810b50a5d
Bernat <bernatgarciacarbo@gmail.com>
2025-05-20 16:06:33 +0200

    Documentation [skip actions]

--- doordeck-sdk/src/jvmMain/kotlin/com/doordeck/multiplatform/sdk/api/SitesApi.jvm.kt
+++ doordeck-sdk/src/jvmMain/kotlin/com/doordeck/multiplatform/sdk/api/SitesApi.jvm.kt
@@ -7,45 +7,54 @@ import com.doordeck.multiplatform.sdk.model.responses.UserForSiteResponse
 import com.doordeck.multiplatform.sdk.util.completableFuture
 import java.util.concurrent.CompletableFuture
 
+/**
+ * Platform-specific implementations of sites-related API calls.
+ */
 actual object SitesApi {
     /**
-     * List sites
-     *
-     * @see <a href="https://developer.doordeck.com/docs/#sites">API Doc</a>
+     * @see SitesClient.listSitesRequest
      */
     suspend fun listSites(): List<SiteResponse> {
         return SitesClient.listSitesRequest()
     }
 
+    /**
+     * Async variant of [SitesApi.listSites] returning [CompletableFuture].
+     */
     fun listSitesAsync(): CompletableFuture<List<SiteResponse>> {
         return completableFuture { listSites() }
     }
 
     /**
-     * Get locks for site
-     *
-     * @see <a href="https://developer.doordeck.com/docs/#get-locks-for-site">API Doc</a>
+     * @see SitesClient.getLocksForSiteRequest
      */
     suspend fun getLocksForSite(siteId: String): List<SiteLocksResponse> {
         return SitesClient.getLocksForSiteRequest(siteId)
     }
 
+    /**
+     * Async variant of [SitesApi.getLocksForSite] returning [CompletableFuture].
+     */
     fun getLocksForSiteAsync(siteId: String): CompletableFuture<List<SiteLocksResponse>> {
         return completableFuture { getLocksForSite(siteId) }
     }
 
     /**
-     * Get users for a site
-     *
-     * @see <a href="https://developer.doordeck.com/docs/#get-users-for-a-site">API Doc</a>
+     * @see SitesClient.getUsersForSiteRequest
      */
     suspend fun getUsersForSite(siteId: String): List<UserForSiteResponse> {
         return SitesClient.getUsersForSiteRequest(siteId)
     }
 
+    /**
+     * Async variant of [SitesApi.getUsersForSite] returning [CompletableFuture].
+     */
     fun getUsersForSiteAsync(siteId: String): CompletableFuture<List<UserForSiteResponse>> {
         return completableFuture { getUsersForSite(siteId) }
     }
 }
 
+/**
+ * Defines the platform-specific implementation of [SitesApi]
+ */
 actual fun sites(): SitesApi = SitesApi
--- doordeck-sdk/src/jvmMain/kotlin/com/doordeck/multiplatform/sdk/api/TilesApi.jvm.kt
+++ doordeck-sdk/src/jvmMain/kotlin/com/doordeck/multiplatform/sdk/api/TilesApi.jvm.kt
@@ -6,34 +6,42 @@ import com.doordeck.multiplatform.sdk.model.responses.TileLocksResponse
 import com.doordeck.multiplatform.sdk.util.completableFuture
 import java.util.concurrent.CompletableFuture
 
+/**
+ * Platform-specific implementations of tile-related API calls.
+ */
 actual object TilesApi {
     /**
-     * Get locks belonging to tile
-     *
-     * @see <a href="https://developer.doordeck.com/docs/#get-locks-belonging-to-tile-v3">API Doc</a>
+     * @see TilesClient.getLocksBelongingToTileRequest
      */
     suspend fun getLocksBelongingToTile(tileId: String): TileLocksResponse {
         return TilesClient.getLocksBelongingToTileRequest(tileId)
     }
 
+    /**
+     * Async variant of [TilesApi.getLocksBelongingToTile] returning [CompletableFuture].
+     */
     fun getLocksBelongingToTileAsync(tileId: String): CompletableFuture<TileLocksResponse> {
         return completableFuture { getLocksBelongingToTile(tileId) }
     }
 
     /**
-     * Associate multiple locks (devices) to a single tile
-     *
-     * @see <a href="https://developer.doordeck.com/docs/#associate-multiple-locks-devices-to-a-single-tile">API Doc</a>
+     * @see TilesClient.associateMultipleLocksRequest
      */
     @SiteAdmin
     suspend fun associateMultipleLocks(tileId: String, siteId: String, lockIds: List<String>) {
         return TilesClient.associateMultipleLocksRequest(tileId, siteId, lockIds)
     }
 
+    /**
+     * Async variant of [TilesApi.associateMultipleLocks] returning [CompletableFuture].
+     */
     @SiteAdmin
     fun associateMultipleLocksAsync(tileId: String, siteId: String, lockIds: List<String>): CompletableFuture<Unit> {
         return completableFuture { associateMultipleLocks(tileId, siteId, lockIds) }
     }
 }
 
+/**
+ * Defines the platform-specific implementation of [TilesApi]
+ */
 actual fun tiles(): TilesApi = TilesApi
