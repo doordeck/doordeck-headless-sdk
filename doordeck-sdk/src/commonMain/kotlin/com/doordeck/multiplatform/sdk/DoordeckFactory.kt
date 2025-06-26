18dfb752a7e30ffa7c6344c7095e979a8f056d23
Bernat <bernatgarciacarbo@gmail.com>
2025-06-26 15:11:29 +0200

    wip

--- doordeck-sdk/src/commonMain/kotlin/com/doordeck/multiplatform/sdk/DoordeckFactory.kt
+++ doordeck-sdk/src/commonMain/kotlin/com/doordeck/multiplatform/sdk/DoordeckFactory.kt
@@ -8,13 +8,13 @@ internal object DoordeckFactory {
 
     fun initialize(sdkConfig: SdkConfig): Doordeck {
         // Add the provided values into the context
-        ContextManagerImpl.apply {
-            setSecureStorageImpl(sdkConfig.secureStorage)
-            setDebugLogging(sdkConfig.debugLogging ?: false)
-            sdkConfig.apiEnvironment?.let { setApiEnvironment(it) }
-            sdkConfig.cloudAuthToken?.let { setCloudAuthToken(it) }
-            sdkConfig.cloudRefreshToken?.let { setCloudRefreshToken(it) }
-            sdkConfig.fusionHost?.let { setFusionHost(it) }
+        ContextManagerImpl.also { context ->
+            context.setSecureStorageImpl(sdkConfig.secureStorage)
+            context.setDebugLogging(sdkConfig.debugLogging ?: false)
+            sdkConfig.apiEnvironment?.let { context.setApiEnvironment(it) }
+            sdkConfig.cloudAuthToken?.let { context.setCloudAuthToken(it) }
+            sdkConfig.cloudRefreshToken?.let { context.setCloudRefreshToken(it) }
+            sdkConfig.fusionHost?.let { context.setFusionHost(it) }
         }
         SdkLogger.d { "Successfully initialized SDK" }
         return DoordeckImpl
