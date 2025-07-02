8628ff40263bcfacc349453902dfd811e83597d8
bernat <bernatgarciacarbo@gmail.com>
2025-07-02 14:01:36 +0200

    wip

--- doordeck-sdk/src/appleTest/kotlin/com/doordeck/multiplatform/sdk/Platform.test.apple.kt
+++ doordeck-sdk/src/appleTest/kotlin/com/doordeck/multiplatform/sdk/Platform.test.apple.kt
@@ -1,7 +1,8 @@
 package com.doordeck.multiplatform.sdk
 
 import kotlinx.cinterop.toKString
+import platform.Foundation.NSProcessInfo
 import platform.posix.getenv
 
 actual fun getEnvironmentVariable(name: String): String? =
-    getenv(name)?.toKString()
+    NSProcessInfo.processInfo.environment[name] as? String ?: getenv(name)?.toKString()
