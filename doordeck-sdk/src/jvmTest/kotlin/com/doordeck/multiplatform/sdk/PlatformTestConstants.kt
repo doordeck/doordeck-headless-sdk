06c806f86920c4638dd65b509447f091bc3a6540
Bernat <bernatgarciacarbo@gmail.com>
2025-07-28 16:20:34 +0200

    RSA wip

--- doordeck-sdk/src/jvmTest/kotlin/com/doordeck/multiplatform/sdk/PlatformTestConstants.kt
+++ doordeck-sdk/src/jvmTest/kotlin/com/doordeck/multiplatform/sdk/PlatformTestConstants.kt
@@ -13,6 +13,7 @@ import com.doordeck.multiplatform.sdk.TestConstants.TEST_SUPPLEMENTARY_USER_ID
 import com.doordeck.multiplatform.sdk.TestConstants.TEST_SUPPLEMENTARY_USER_PUBLIC_KEY
 import com.doordeck.multiplatform.sdk.crypto.CryptoManager.toPrivateKey
 import com.doordeck.multiplatform.sdk.crypto.CryptoManager.toPublicKey
+import com.doordeck.multiplatform.sdk.crypto.CryptoManager.toRsaPublicKey
 import com.doordeck.multiplatform.sdk.util.Utils.decodeBase64ToByteArray
 import com.doordeck.multiplatform.sdk.util.toUuid
 
@@ -22,11 +23,11 @@ internal object PlatformTestConstants {
     val PLATFORM_TEST_MAIN_USER_PUBLIC_KEY by lazy { TEST_MAIN_USER_PUBLIC_KEY.decodeBase64ToByteArray().toPublicKey() }
     val PLATFORM_TEST_MAIN_USER_ID by lazy { TEST_MAIN_USER_ID.toUuid() }
     val PLATFORM_TEST_SUPPLEMENTARY_USER_ID by lazy { TEST_SUPPLEMENTARY_USER_ID.toUuid() }
-    val PLATFORM_TEST_SUPPLEMENTARY_USER_PUBLIC_KEY by lazy { TEST_SUPPLEMENTARY_USER_PUBLIC_KEY.decodeBase64ToByteArray().toPublicKey() }
+    val PLATFORM_TEST_SUPPLEMENTARY_USER_PUBLIC_KEY by lazy { TEST_SUPPLEMENTARY_USER_PUBLIC_KEY.toRsaPublicKey() }
     val PLATFORM_TEST_MAIN_TILE_ID by lazy { TEST_MAIN_TILE_ID.toUuid() }
     val PLATFORM_TEST_MAIN_LOCK_ID by lazy { TEST_MAIN_LOCK_ID.toUuid() }
     val PLATFORM_TEST_MAIN_SITE_ID by lazy { TEST_MAIN_SITE_ID.toUuid() }
     val PLATFORM_TEST_SUPPLEMENTARY_TILE_ID by lazy { TEST_SUPPLEMENTARY_TILE_ID.toUuid() }
     val PLATFORM_TEST_SUPPLEMENTARY_SECOND_USER_ID by lazy { TEST_SUPPLEMENTARY_SECOND_USER_ID.toUuid() }
-    val PLATFORM_TEST_SUPPLEMENTARY_SECOND_USER_PUBLIC_KEY by lazy { TEST_SUPPLEMENTARY_SECOND_USER_PUBLIC_KEY.decodeBase64ToByteArray().toPublicKey() }
+    val PLATFORM_TEST_SUPPLEMENTARY_SECOND_USER_PUBLIC_KEY by lazy { TEST_SUPPLEMENTARY_SECOND_USER_PUBLIC_KEY.toRsaPublicKey() }
 }
