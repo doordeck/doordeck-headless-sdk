1af418a52487527aa6fdc23ca41591f87673cc25
Bernat <bernatgarciacarbo@gmail.com>
2025-09-08 20:00:47 +0200

    Improve JS platform detection

--- doordeck-sdk/src/jsMain/kotlin/com/doordeck/multiplatform/sdk/Platform.js.kt
+++ doordeck-sdk/src/jsMain/kotlin/com/doordeck/multiplatform/sdk/Platform.js.kt
@@ -1,10 +1,12 @@
 package com.doordeck.multiplatform.sdk
 
 actual val platformType by lazy {
-    return@lazy if (isNode) {
+    if (isNode) {
         PlatformType.JS_NODE
-    } else {
+    } else if (isBrowser) {
         PlatformType.JS_BROWSER
+    } else {
+        PlatformType.JS
     }
 }
 
@@ -12,3 +14,5 @@ internal actual object ApplicationContext
 
 private val isNode: Boolean =
     js("typeof process !== 'undefined' && process.versions != null && process.versions.node != null") as Boolean
+
+private val isBrowser: Boolean = js("typeof window !== 'undefined'") as Boolean
