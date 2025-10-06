b656b0b1b938ac2057c2c7b8b3da4e7a3749a9d5
Bernat <bernatgarciacarbo@gmail.com>
2025-10-06 22:13:02 +0200

    wip

--- samples/wpf/WpfSample/Converters/DateTimeToLocalTimeConverter.cs
+++ samples/wpf/WpfSample/Converters/DateTimeToLocalTimeConverter.cs
@@ -3,13 +3,11 @@ using System.Windows.Data;
 
 namespace WpfSample.Converters;
 
-public class TimestampToTextConverter : IValueConverter
+public class DateTimeToLocalTimeConverter : IValueConverter
 {
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        return value is not double timestamp ? 
-            string.Empty : 
-            DateTimeOffset.FromUnixTimeSeconds((long)timestamp).UtcDateTime.ToString("R");
+        return value is DateTime dt ? dt.ToLocalTime() : string.Empty;
     }
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
