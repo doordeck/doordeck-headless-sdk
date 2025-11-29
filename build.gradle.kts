9cfbd9b8899b3d4c5b4231b989ccf1bccd29f68b
bernat <bernatgarciacarbo@gmail.com>
2025-11-29 15:40:58 +0100

    Solving security reports

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,5 +33,6 @@ rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlu
     rootProject.the<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension>().apply {
         resolution("brace-expansion", "2.0.2")
         resolution("node-forge", "1.3.2")
+        resolution("js-yaml", "4.1.1")
     }
 }
