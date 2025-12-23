43d111776a4a0fedc27af2f039c1f582c1bc6e94
Bernat <bernatgarciacarbo@gmail.com>
2025-12-23 15:42:59 +0100

    yarn.lock update [skip actions]

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,5 @@ nexusPublishing {
 rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
     rootProject.the<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension>().apply {
         resolution("brace-expansion", "2.0.2")
-        resolution("node-forge", "1.3.2")
-        resolution("js-yaml", "4.1.1")
     }
 }
