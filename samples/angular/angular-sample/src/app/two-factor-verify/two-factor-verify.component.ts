b435f7d2b4c85a4d7704791eab9f12d6493d2f2b
Bernat Carbó <41543253+BernatCarbo@users.noreply.github.com>
2026-01-15 13:30:59 +0100

    Bump sample deps [skip actions] (#335)

--- samples/angular/angular-sample/src/app/two-factor-verify/two-factor-verify.component.ts
+++ samples/angular/angular-sample/src/app/two-factor-verify/two-factor-verify.component.ts
@@ -4,22 +4,21 @@ import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/
 import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {MatButton} from '@angular/material/button';
-import {NgIf} from '@angular/common';
+
 
 @Component({
     selector: 'app-two-factor-verify',
     imports: [
-        MatDialogTitle,
-        MatDialogContent,
-        ReactiveFormsModule,
-        MatFormField,
-        MatDialogActions,
-        MatInput,
-        MatButton,
-        MatLabel,
-        NgIf,
-        MatError
-    ],
+    MatDialogTitle,
+    MatDialogContent,
+    ReactiveFormsModule,
+    MatFormField,
+    MatDialogActions,
+    MatInput,
+    MatButton,
+    MatLabel,
+    MatError
+],
     templateUrl: './two-factor-verify.component.html',
     styleUrl: './two-factor-verify.component.css'
 })
