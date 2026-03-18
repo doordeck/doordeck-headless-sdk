c7070b00b6d808055a2658ccf89910caa43a22f3
Bernat Carbó <41543253+BernatCarbo@users.noreply.github.com>
2026-03-18 19:26:06 +0100

    Fixing deprecated warnings [skip actions] (#385)

--- doordeck-sdk/src/commonMain/kotlin/com/doordeck/multiplatform/sdk/util/Utils.kt
+++ doordeck-sdk/src/commonMain/kotlin/com/doordeck/multiplatform/sdk/util/Utils.kt
@@ -5,7 +5,11 @@ import kotlin.js.JsExport
 
 @JsExport
 object Utils {
-    fun String.decodeBase64ToByteArray(): ByteArray = Base64.decode(this)
+    fun String.decodeBase64ToByteArray(): ByteArray = runCatching {
+        Base64.withPadding(Base64.PaddingOption.PRESENT_OPTIONAL).decode(this)
+    }.getOrElse {
+        Base64.UrlSafe.withPadding(Base64.PaddingOption.PRESENT_OPTIONAL).decode(this)
+    }
 
     fun ByteArray.encodeByteArrayToBase64(): String = Base64.encode(this)
 
