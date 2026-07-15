34aa60a66357b6cc4effb879ba54176d6ac49449
Bernat <bernatgarciacarbo@gmail.com>
2026-07-15 18:40:13 +0200

    missing audit user agent types

--- doordeck-sdk/src/mingwMain/resources/csharp/Model/AuditUserAgentType.cs
+++ doordeck-sdk/src/mingwMain/resources/csharp/Model/AuditUserAgentType.cs
@@ -7,11 +7,14 @@ public enum AuditUserAgentType
 {
     KMP_SDK_JVM,
     KMP_SDK_ANDROID,
+    KMP_SDK_APPLE,
     KMP_SDK_APPLE_MAC,
     KMP_SDK_APPLE_IOS,
     KMP_SDK_APPLE_WATCH,
     KMP_SDK_WINDOWS,
+    KMP_SDK_JS,
     KMP_SDK_JS_BROWSER,
+    KMP_SDK_JS_NODE,
     FUSION,
     PKOC_CONNECTOR
 }
