3bd8dfa33e0abd8115b38eb5ac5ae40a8d929eb5
bernat <bernatgarciacarbo@gmail.com>
2026-08-16 05:31:14 +0200

    wip

--- doordeck-sdk/src/androidHostTest/kotlin/com/doordeck/multiplatform/sdk/PlatformTestConstants.kt
+++ doordeck-sdk/src/androidHostTest/kotlin/com/doordeck/multiplatform/sdk/PlatformTestConstants.kt
@@ -2,10 +2,6 @@ package com.doordeck.multiplatform.sdk
 
 import com.doordeck.multiplatform.sdk.TestConstants.FUSION_INTEGRATIONS
 import com.doordeck.multiplatform.sdk.TestConstants.TEST_EXPIRED_CERTIFICATE
-import com.doordeck.multiplatform.sdk.TestConstants.TEST_MAIN_APPLICATION_NAME
-import com.doordeck.multiplatform.sdk.TestConstants.TEST_MAIN_FUSION_DOOR_NAME
-import com.doordeck.multiplatform.sdk.TestConstants.TEST_MAIN_LOCK_NAME
-import com.doordeck.multiplatform.sdk.TestConstants.TEST_MAIN_USER_EMAIL
 import com.doordeck.multiplatform.sdk.TestConstants.TEST_MAIN_USER_PRIVATE_KEY
 import com.doordeck.multiplatform.sdk.TestConstants.TEST_MAIN_USER_PUBLIC_KEY
 import com.doordeck.multiplatform.sdk.TestConstants.TEST_SUPPLEMENTARY_SECOND_USER_ID
@@ -31,10 +27,6 @@ internal object PlatformTestConstants {
 
     val PLATFORM_TEST_MAIN_USER_PRIVATE_KEY by lazy { TEST_MAIN_USER_PRIVATE_KEY.decodeBase64ToByteArray().toPrivateKey() }
     val PLATFORM_TEST_MAIN_USER_PUBLIC_KEY = TEST_MAIN_USER_PUBLIC_KEY.decodeBase64ToByteArray().toPublicKey()
-    val PLATFORM_TEST_MAIN_APPLICATION_NAME = TEST_MAIN_APPLICATION_NAME
-    val PLATFORM_TEST_MAIN_FUSION_DOOR_NAME = TEST_MAIN_FUSION_DOOR_NAME
-    val PLATFORM_TEST_MAIN_LOCK_NAME = TEST_MAIN_LOCK_NAME
-    val PLATFORM_TEST_MAIN_USER_EMAIL = TEST_MAIN_USER_EMAIL
     val PLATFORM_TEST_MAIN_USER_ID = PLATFORM_MAIN_USER_ID.toUuid()
     val PLATFORM_TEST_SUPPLEMENTARY_USER_ID = TEST_SUPPLEMENTARY_USER_ID.toUuid()
     val PLATFORM_TEST_SUPPLEMENTARY_USER_PUBLIC_KEY = TEST_SUPPLEMENTARY_USER_PUBLIC_KEY.toRsaPublicKey()
--- doordeck-sdk/src/androidHostTest/kotlin/com/doordeck/multiplatform/sdk/api/PlatformApiTest.android.kt
+++ doordeck-sdk/src/androidHostTest/kotlin/com/doordeck/multiplatform/sdk/api/PlatformApiTest.android.kt
@@ -1,10 +1,10 @@
 package com.doordeck.multiplatform.sdk.api
 
 import com.doordeck.multiplatform.sdk.IntegrationTest
-import com.doordeck.multiplatform.sdk.PlatformTestConstants.PLATFORM_TEST_MAIN_APPLICATION_NAME
-import com.doordeck.multiplatform.sdk.PlatformTestConstants.PLATFORM_TEST_MAIN_USER_EMAIL
 import com.doordeck.multiplatform.sdk.PlatformTestConstants.PLATFORM_TEST_MAIN_USER_ID
 import com.doordeck.multiplatform.sdk.PlatformTestConstants.PLATFORM_TEST_SUPPLEMENTARY_USER_ID
+import com.doordeck.multiplatform.sdk.TestConstants.TEST_MAIN_APPLICATION_NAME
+import com.doordeck.multiplatform.sdk.TestConstants.TEST_MAIN_USER_EMAIL
 import com.doordeck.multiplatform.sdk.TestConstants.TEST_MAIN_USER_PASSWORD
 import com.doordeck.multiplatform.sdk.context.ContextManager
 import com.doordeck.multiplatform.sdk.crypto.CryptoManager
@@ -46,9 +46,9 @@ class PlatformApiTest : IntegrationTest() {
 
     @After
     fun cleanUp() = runBlocking {
-        AccountlessApi.login(PLATFORM_TEST_MAIN_USER_EMAIL, TEST_MAIN_USER_PASSWORD)
+        AccountlessApi.login(TEST_MAIN_USER_EMAIL, TEST_MAIN_USER_PASSWORD)
         PlatformApi.listApplications().filter { application ->
-            application.name.startsWith(PLATFORM_TEST_MAIN_APPLICATION_NAME) &&
+            application.name.startsWith(TEST_MAIN_APPLICATION_NAME) &&
                     application.owners.any { it == PLATFORM_TEST_MAIN_USER_ID }
         }.forEach { application ->
             PlatformApi.deleteApplication(application.applicationId)
@@ -58,9 +58,9 @@ class PlatformApiTest : IntegrationTest() {
     @Test
     fun shouldTestPlatform() = runTest(timeout = 2.minutes) {
         // Given - shouldCreateApplication
-        val authTokens = AccountlessApi.login(PLATFORM_TEST_MAIN_USER_EMAIL, TEST_MAIN_USER_PASSWORD)
+        val authTokens = AccountlessApi.login(TEST_MAIN_USER_EMAIL, TEST_MAIN_USER_PASSWORD)
         val newApplication = PlatformOperations.CreateApplication(
-            name = "$PLATFORM_TEST_MAIN_APPLICATION_NAME - ${randomUuidString()}",
+            name = "$TEST_MAIN_APPLICATION_NAME - ${randomUuidString()}",
             companyName = randomString(),
             mailingAddress = randomEmail(),
             privacyPolicy = randomUri(),
@@ -83,7 +83,7 @@ class PlatformApiTest : IntegrationTest() {
         assertEquals(newApplication.supportContact, application.supportContact)
 
         // Given - shouldUpdateApplicationName
-        val updatedApplicationName = "$PLATFORM_TEST_MAIN_APPLICATION_NAME - ${randomUuidString()}"
+        val updatedApplicationName = "$TEST_MAIN_APPLICATION_NAME - ${randomUuidString()}"
 
         // When
         PlatformApi.updateApplicationName(application.applicationId, updatedApplicationName)
